ab0d2da6762ae401415c63bbb0c209d70db3696c
Wesley de Groot <email@wesleydegroot.nl>
2024-05-29 13:16:13 +0200

    Update HTTPMethod.swift

--- Sources/SimpleNetworking/Structs/HTTPMethod.swift
+++ Sources/SimpleNetworking/Structs/HTTPMethod.swift
@@ -13,31 +13,39 @@ import Foundation
 extension SimpleNetworking {
     /// HTTP Methods
     public enum HTTPMethod {
-        /// HTTP GET
+        /// The `GET` method requests a representation of the specified resource. 
+        /// Requests using `GET` should only retrieve data.
         case get
 
-        /// HTTP PUT
+        /// The `PUT` method replaces all current representations of the target resource with the request payload.
         ///
         /// - parameters:
         ///   - value: A value to put
         case put(Any?)
 
-        /// HTTP POST
+        /// The `POST` method submits an entity to the specified resource,
+        /// often causing a change in state or side effects on the server.
         ///
         /// - parameters:
         ///   - value: A value to post
         case post(Any?)
 
-        /// HTTP DELETE
+        /// The `DELETE` method deletes the specified resource.
         ///
         /// - parameters:
         ///   - value: A value to delete
         case delete(Any?)
 
+        /// The `PATCH` method applies partial modifications to a resource.
+        ///
+        /// - parameters:
+        ///   - value: A value to delete
+        case patch(Any?)
+
         /// Method for internal use
         ///
         /// Method used for the HTTP Request.
-        /// This can be `GET`, `PUT`, `POST`, `DELETE`.
+        /// This can be `GET`, `PUT`, `POST`, `PATCH`, `DELETE`.
         var method: String {
             switch self {
             case .get:
@@ -46,6 +54,8 @@ extension SimpleNetworking {
                 return "PUT"
             case .post:
                 return "POST"
+            case .patch:
+                return "PATCH"
             case .delete:
                 return "DELETE"
             }
