5340349299321a7d1dc3f9fbb448afe932f8daee
Wesley de Groot <email@wesleydegroot.nl>
2024-07-08 22:33:02 +0200

    add PrivacyInfo.xcprivacy as resource

--- Package.swift
+++ Package.swift
@@ -21,6 +21,9 @@ let package = Package(
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "SimpleNetworking",
+            resources: [
+                .process("PrivacyInfo.xcprivacy")
+            ],
             swiftSettings: [
                 .enableExperimentalFeature("StrictConcurrency")
             ]
